572df3784604235bc708334d64c9fd6169500cef
DkS <khanh34@gmail.com>
2018-06-30 13:47:38 +0700

    upload model

--- evaluate.py
+++ evaluate.py
@@ -2,6 +2,81 @@ from model.data_utils import CoNLLDataset
 from model.ner_model import NERModel
 from model.config import Config
 
+RESPONSE = {
+    'Amount':None,
+    'Merchant': None,
+	'Date': None,
+	'Address': None
+}
+
+KEYS = ['Amount', 'Merchant', 'Date', 'Address']
+PREFIX_BIO = ['B', 'I']
+
+TEXT_STR = '521 Hoang Van\n Hoang Van Thu Q.Tan Binh\n DON\n 23/06/2018(Thu\n 08:10\n Temakionigiri\n 16.000\n 16,000*\n tinh:\n cong:\n 16,000\n iue\n So don:2-69318 Thu ngan:120013\n khach nhu cau xuat hca don do,\n vien toi\n '
+
+TEXT_STR_1 = '521 Hoang Van Hoang Van Thu Q.Tan Binh'
+
+TEXT_STR_2 = 'CIRCLE K VIETNAM\n 124 Pho Quang. W.9. Phu Nhuan.D. HCM\n Receipt No: 088 02 20180628 0314\n Date: jun, 29 2018 06:12 PM\n Jun 28,\n Cashier: 5473-Diem Tan Thi Le\n Description:\n 1 NESTEA Tea LemON Small 120z*1CP 7,000\n 1 Item(s) (VAT included) 7,000\n CASH\n 7,000\n CHANGE\n '
+
+TEXT_STR_3 = 'CIRCLE K VIETNAM\n\
+968 3/2 Street. Ward 15 Dist 11 HCMC\n\
+Receipt No: 153 02 20180630 0046\n\
+Date: Jun 30, 2018 07:37 AM\n\
+Cashier: 5022-Vi Ngo Nguyen Yen\n\
+Description:\n\
+CK Mixed Drinkime Soda\n\
+Noodle with Fried Egg 14,000\n\
+DC//C+ VTM C 350 6,000\n\
+2 Item(s) (VAT included) 20,000\n\
+20,000\n\
+CASH\n\
+CHANGE\n\
+'
+
+def get_data_from_key(data, key):
+    output = []
+
+    # B to I
+    prefix_key = 'B-' + key
+    for idx, pred in enumerate(data['output']):
+        if pred == prefix_key:
+            output.append(data['input'][idx])
+            found = False
+            prefix_key_i = 'I-' + key
+            for i, pred_i in enumerate(data['output'][idx:]):
+                if pred_i == prefix_key_i:
+                    found = True
+                    output.append(data['input'][i + idx])
+                    print(data['input'][i + idx], i, idx)
+                elif found == True:
+                    break
+            break
+
+    if len(output) > 0:
+        return ' '.join(output)
+
+    # I only
+    prefix_key = 'I-' + key
+    found = False
+    for idx, pred in enumerate(data['output']):
+        if pred == prefix_key:
+            found = True
+            output.append(data['input'][idx])
+        elif found == True:
+            return ' '.join(output)
+
+
+
+
+    for prefix in PREFIX_BIO:
+        prefix_key = prefix + '-' + key
+        print(prefix_key)
+        for idx, pred in enumerate(data['output']):
+            if pred == prefix_key:
+                print(idx)
+                output.append(data['input'][idx])
+
+    return output
 
 def align_data(data):
     """Given dict with lists, creates aligned strings
@@ -28,6 +103,8 @@ def align_data(data):
             str_aligned += token + " " * (spacing - len(token) + 1)
 
         data_aligned[key] = str_aligned
+        
+        print(seq)
 
     return data_aligned
 
@@ -60,10 +137,16 @@ input> I love Paris""")
             break
 
         preds = model.predict(words_raw)
-        to_print = align_data({"input": words_raw, "output": preds})
+        # to_print = align_data({"input": words_raw, "output": preds})
+        model.logger.info(preds)
+        print(preds)
+        # for key in KEYS:
+        #     print(key)
+        #     value = get_data_from_key({'input': words_raw, 'output': preds}, key)
+        #     print(value)
 
-        for key, seq in to_print.items():
-            model.logger.info(seq)
+        # for key, seq in to_print.items():
+            # model.logger.info(seq)
 
 
 def main():
@@ -80,9 +163,30 @@ def main():
                          config.processing_tag, config.max_iter)
 
     # evaluate and interact
-    model.evaluate(test)
-    interactive_shell(model)
+    # DkS, stop evaluate
+    # model.evaluate(test)
+    # interactive_shell(model)
+    test_string = TEXT_STR_3
+    lines = test_string.split('\n')
+    words = []
+    prediction = []
+    for line in lines:
+        token = line.strip().split(' ')
+        preds = model.predict(token)
+        words = words + token
+        prediction = prediction + preds
 
+    # print(words)
+    # print(prediction)
+    response = {}
+    for key in KEYS:
+        print(key)
+        value = get_data_from_key({'input': words, 'output': prediction}, key)
+        if len(value) > 0:
+            response[key] = value
+
+
+    print(response)
 
 if __name__ == "__main__":
     main()
